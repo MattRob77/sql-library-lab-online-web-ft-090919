941b2e4a58ef0ee3ed2482617039b6e6407cc448
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-11 01:00:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,11 @@ end
 
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series 
+  JOIN characters ON characters.author_id = series.author_id
+  GROUP BY series.title 
+  ORDER BY characters.species DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
