93ef3d1f78aaf7b13681074b04a929a7ac702e87
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-11 00:43:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,12 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 199
 (5, "Harry Potter prisoner of azkaban", 1999, 3), (6, "Harry Potter and the goblet of fire", 2000, 4);
 
 
+INSERT INTO characters (id, name, motto, species, author_id) VALUES
+(1, "Holden", "Coffee?", "Human", 1),
+(2, "Naomi", "Belta-lowda", "Human", 1),
+(3, "Alex", "Y'all", "Human", 1),
+(4, "Amos", "I don't feel feelings", "Human", 1),
+(5, "Frodo", "Alright then, keep your secrets", "Hobbit", 2),
+(6, "Legolas", "You have my bow", "Elf", 2),
+(7, "Gimli", "You have my axe", "Dwarf", 2),
+(8, "Gandalf", "It's 'Gandalf the White' now, bitches", "Wizard", 2);
