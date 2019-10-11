0cc4849b57d828c398d0e02668130697814fe13c
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-11 00:42:54 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Game of Thrones", 1, 1), (2, "Fire and Ice", 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "science fiction");
+
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Jason Momoa");
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "Gladiator", 1930, 1), (3, "Harry Potter and the Philosopher's Stone ", 1997, 1), (4, "Harry Potter and the Chamber of Secrets", 1998, 2),
+(5, "Harry Potter prisoner of azkaban", 1999, 3), (6, "Harry Potter and the goblet of fire", 2000, 4);
+
+
